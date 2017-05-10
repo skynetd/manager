59a2946f9d6c1ef985e2529a2faa93c03fb83b04
skynetd <skynet.daemon@gmail.com>
2017-05-09 21:44:31 -0500

    Stephen Grider's Manager Tutorial

---
+++ index.ios.js
@@ -0,0 +1,4 @@
+import {AppRegistry} from 'react-natvie';
+import App from './src/app';
+
+AppRegistry.registerComponent('manager', ()=>App);
---
+++ src/actions/index.js
@@ -0,0 +1,8 @@
+export const action = ()=>{
+    
+    return{
+        type:
+        payload:
+    }
+    
+}
